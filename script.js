e1cf3443c2ed6d436cf81a6150ca5f7166f59b2a
Djoulita schydlowsky <djoulitaa.schyd@gmail.com>
2025-11-07 23:24:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('nav a, .btn-action');
-
-    navLinks.forEach(link => {
-        link.addEventListener('mouseenter', () => {
-            
-            link.classList.add('glitch');
-            setTimeout(() => {
-                link.classList.remove('glitch');
-            }, 300); // 0.3 seconde
-        });
-    });
-});
-
-
